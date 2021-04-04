927bdf73a3b1f419bdc5b17022511a8888fbd954
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-04-04 17:08:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function get_Paragraph1(){
+    p1s1 = document.getElementById("para1_input1").value;
+    p1s2 = document.getElementById("para1_input2").value;
+    p1s3 = document.getElementById("para1_input3").value;
+    p1s4 = document.getElementById("para1_input4").value;
+    p1s5 = document.getElementById("para1_input5").value;
+    p1s6 = document.getElementById("para1_input6").value;
+    document.getElementById("para1").innerHTML = p1s1+p1s2+p1s3+p1s4+p1s5+p1s6;
+};
+function get_Paragraph2(){
+    p2s1 = document.getElementById("para2_input1").value;
+    p2s2 = document.getElementById("para2_input2").value;
+    p2s3 = document.getElementById("para2_input3").value;
+    p2s4 = document.getElementById("para2_input4").value;
+    p2s5 = document.getElementById("para2_input5").value;
+    p2s6 = document.getElementById("para2_input6").value;
+    document.getElementById("para2").innerHTML = p2s1+p2s2+p2s3+p2s4+p2s5+p2s6;
+};
+
+
+
